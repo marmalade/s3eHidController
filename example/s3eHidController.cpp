6c5e1a838eae8b5ac78792f762d6edab95efd6e9
Nick <nickraymondsmith@gmail.com>
2015-01-12 14:45:51 +0000

    Rename and refactor
    Renamed from 360Controller/GamePad to s3eHidController. Updated readme.
    Rearanged folders. Added legacy GamePad.h to allow use of old names
    still. Added .gitignore. Built extension libs and checked them in so
    people can actually use it out of the box!

---
+++ example/s3eHidController.cpp
@@ -0,0 +1,77 @@
+
+// Include the single header file for the IwGx module
+#include "IwGx.h"
+#include "s3eHidController.h"
+
+// Standard C-style entry point. This can take args if required.
+int main()
+{
+    // Initialise the IwGx drawing module
+    IwGxInit();
+
+    // Set the background colour to (opaque) blue
+    IwGxSetColClear(0, 0, 0xff, 0xff);
+
+    // Loop forever, until the user or the OS performs some action to quit the app
+    while (!s3eDeviceCheckQuitRequest())
+    {
+        // Update the Xbox 360 game-pad, enabling button latch, input event processing etc
+        s3eHidControllerUpdate(0.0f);
+
+        // Clear the surface
+        IwGxClear();
+
+        // Use the built-in font to display a string at coordinate (120, 150)
+        const int MaxStr = 255;
+        char str[MaxStr] = "";
+        snprintf(str, MaxStr, "Left-Stick: {X=%.3f, Y=%.3f}",s3eHidControllerGetStick1XAxis(), s3eHidControllerGetStick1YAxis());
+        IwGxPrintString(10, 150, str);
+        snprintf(str, MaxStr, "Right-Stick: {X=%.3f, Y=%.3f}", s3eHidControllerGetStick2XAxis(), s3eHidControllerGetStick2YAxis());
+        IwGxPrintString(10, 170, str);
+        snprintf(
+            str, 
+            MaxStr, 
+            "%s %s %s %s", 
+            s3eHidControllerGetButtonA() ? "A" : "",
+            s3eHidControllerGetButtonB() ? "B" : "",
+            s3eHidControllerGetButtonX() ? "X" : "",
+            s3eHidControllerGetButtonY() ? "Y" : "");
+        IwGxPrintString(10, 190, str);
+        snprintf(
+            str, 
+            MaxStr, 
+            "%s %s %s %s",
+            s3eHidControllerGetButtonSelect() ? "Select" : "",
+            s3eHidControllerGetButtonStart() ? "Start" : "",
+            s3eHidControllerGetButtonLShoulderDown() ? "LShoulder" : "",
+            s3eHidControllerGetButtonRShoulderDown() ? "RShoulder" : "");
+        IwGxPrintString(10, 210, str);
+        snprintf(str, MaxStr, "Left-Trigger: %.3f Right-Trigger: %.3f}", s3eHidControllerGetLeftTrigger(), s3eHidControllerGetRightTrigger());
+        IwGxPrintString(10, 230, str);
+        snprintf(
+            str, 
+            MaxStr, 
+            "%s %s %s %s", 
+            s3eHidControllerGetButtonDPadLeft() ? "DPadLeft" : "",
+            s3eHidControllerGetButtonDPadRight() ? "DPadRight" : "",
+            s3eHidControllerGetButtonDPadUp() ? "DPadUp" : "",
+            s3eHidControllerGetButtonDPadDown() ? "DPadDown" : "");
+        IwGxPrintString(10, 250, str);
+
+        
+        // Standard EGL-style flush of drawing to the surface
+        IwGxFlush();
+
+        // Standard EGL-style flipping of double-buffers
+        IwGxSwapBuffers();
+
+        // Sleep for 0ms to allow the OS to process events etc.
+        s3eDeviceYield(0);
+    }
+
+    // Shut down the IwGx drawing module
+    IwGxTerminate();
+
+    // Return
+    return 0;
+}
